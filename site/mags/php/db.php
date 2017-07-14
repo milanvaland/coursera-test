34b73eaf75cbd41fd62dbdcd782dcb7ef74d0cb4
milanvaland <valandmilan3@gmail.com>
2017-07-14 15:34:42 +0530

    file delete

--- site/mags/php/db.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$user = "milan" ;
-	$password = "milan";
-	$db = new PDO('mysql:host=localhost;dbname=ldMagz',$user,$password);
-?>
--- site/mags/php/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-	#session_resume
-	session_start();
-	session_destroy();
-	header('Location:../pages/LoginPage.php');
-?>
